8feb3ffb0b4452a8d4cfec2b038a6f94fc2a4c62
Erika-tech257 <erikajtech@gmail.com>
2021-11-23 17:52:46 -0500

    created User Profile DTO, created methods in UserInfo Service, added Post request in UserInfoController

--- user-service/src/main/java/com/revature/nova/models/UserInfoModel.java
+++ user-service/src/main/java/com/revature/nova/models/UserInfoModel.java
@@ -1,10 +1,16 @@
 package com.revature.nova.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.revature.nova.DTOs.UserProfileDTO;
 import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+
+/**
+ * @author Erika Johnson
+ * Added 3 fields to model ( state, favorite_genre, message) and custom args constructor
+ */
 
 @Entity
 @Table(name = "user_info")
@@ -15,6 +21,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @ToString
 public class UserInfoModel implements Serializable {
+
     @Id
     @Column(name = "userinfo_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,9 +33,28 @@ public class UserInfoModel implements Serializable {
     @Column(name = "password")
     private String password;
 
-    @Column(name = "email_id")
+    @Column
     private String email;
+
+    @Column
+    private String state;
+
+    @Column
+    private String favorite_genre;
+
+    @Column
+    private String message;
+
+    public UserInfoModel(String username, String email, String state, String favorite_genre, String message) {
+        this.username = username;
+        this.email = email;
+        this.state = state;
+        this.favorite_genre = favorite_genre;
+        this.message = message;
+    }
 
     @OneToOne(mappedBy = "userInfoModel")
     UserModel userModel;
+
+
 }
