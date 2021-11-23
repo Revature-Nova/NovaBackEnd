f5a70b128c5ee07afa5d234f7df4cf98b59aea0b
TCON163 <tylerconnercareersearch@gmail.com>
2021-11-22 19:05:07 -0600

    fixed DiscoverySericeApplication

--- discovery-service/src/main/java/com/revature/nova/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/revature/nova/DiscoveryServiceApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryService {
+@SpringBootApplication
+public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryService.class, args);
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
 }
