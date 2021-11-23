044cf54d64835b0825e53d66913b9a0e2664fe00
BrittLowell <brittany.lowell@revature.net>
2021-11-23 15:22:40 -0700

    working on ProductService

--- product-service/src/main/java/com/revature/nova/services/ProductService.java
+++ product-service/src/main/java/com/revature/nova/services/ProductService.java
@@ -1,9 +1,12 @@
 package com.revature.nova.services;
 
+import com.revature.nova.models.Product;
 import com.revature.nova.repositories.ProductRepo;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -15,9 +18,40 @@ public class ProductService {
     }
 
     /**
-     * This method gets the products from the database
+     * This method gets all products.
+     *
+     * @return Returns a list containing all products
      */
-    public void getProduct(){
-        repo.findAll();
+    public List<Product> displayAllProducts(){
+        return repo.findAll();
+    }
+
+    /**
+     * This method returns a filtered list of products using the given filter type and filter value.
+     * The filter type and filter value need to match what is in the database exactly.
+     *
+     * @param type This inputs determines how the products will be filtered. The valid filter types are:
+     *             genre, platform, and rating.
+     * @param value This input determines the value the products will be filtered by. Valid filter values include:
+     *              For genre: RPG, Action, Horror, etc.
+     *              For platform: PlayStation 4, Switch, etc.
+     *              For rating: Mature, E10+, Teen, etc.
+     * @return Returns a list of filtered products.
+     */
+    public List<Product> filterProducts(String type, String value){
+        List<Product> productList = new ArrayList<>();
+
+        switch (type) {
+            case "genre":
+                productList = repo.findByGenre(value);
+                break;
+            case "platform":
+                productList = repo.findByPlatform(value);
+                break;
+            case "rating":
+                productList = repo.findByRating(value);
+                break;
+        }
+        return productList;
     }
 }
