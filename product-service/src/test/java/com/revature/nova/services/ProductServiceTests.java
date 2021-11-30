c0024c24ac31defe7e8613bd376ae5a3a7d3a86c
BrittLowell <brittany.lowell@revature.net>
2021-11-30 16:04:50 -0700

    fixed tests so that they are more robust and actually test what I want to be tested

--- product-service/src/test/java/com/revature/nova/services/ProductServiceTests.java
+++ product-service/src/test/java/com/revature/nova/services/ProductServiceTests.java
@@ -4,15 +4,19 @@ import com.revature.nova.models.Product;
 import com.revature.nova.repositories.ProductRepo;
 import com.revature.nova.services.ProductService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockSettings;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.parameters.P;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -24,25 +28,39 @@ import static org.mockito.Mockito.*;
 class ProductServiceTests {
     private ProductService productService;
     private List<Product> testProductList;
+    private List<Product> mockDatabaseData;
 
     @Mock
-    private ProductService mockProductService;
+    private ProductRepo mockProductRepo;
 
-    @Mock
-    private ProductRepo productRepo;
+    @BeforeEach
+    void setUp() {
+//        MockitoAnnotations.initMocks(this); depreciated, but might need it
+        productService = new ProductService(mockProductRepo);
+        Product product = new Product(119,"The Legend of Zelda: Breath of the Wild", "RPG", 59.99f, "E10+", "https://rawg.io/api/games/the-legend-of-zelda-breath-of-the-wild?key=87ad23cdc737468884eb0216a7ba8df9:", "Nintendo Switch", "https://imgur.com/onC0oCn");
+        Product product1 = new Product(120, "Subnautica", "Adventure", 29.99f, "E10+", "https://rawg.io/api/games/subnautica?key=87ad23cdc737468884eb0216a7ba8df9", "PlayStation 4", "https://imgur.com/JkX9r1e");
+        Product product2 = new Product(121, "The Legend of Zelda: Skyward Sword", "Adventure", 19.99f, "E10+", "https://rawg.io/api/games/the-legend-of-zelda-skyward-sword?key=87ad23cdc737468884eb0216a7ba8df9", "Wii", "https://imgur.com/VvU45oV");
+        Product product3 = new Product(122,"Thief", "Stealth", 21.27f, "Mature", "https://rawg.io/api/games/thief?key=87ad23cdc737468884eb0216a7ba8df9", "PlayStation 3", "https://imgur.com/Z0EPE84");
+        List<Product> temp = new ArrayList<>();
+        temp.add(product);
+        temp.add(product1);
+        temp.add(product2);
+        temp.add(product3);
+        setMockDatabaseData(temp);
+    }
 
-    @Autowired
-    public ProductServiceTests(ProductService productService) {
-        this.productService = productService;
+    @AfterEach
+    void tearDown() {
+        productService = null;
     }
 
     /**
      * This test checks if display all products returns a list that is not empty when the database is not empty.
      **/
     @Test
-
     public void Test_successfullyDisplayAllProducts() {
-        //Arrange: set up in constructor
+        //Arrange
+        doReturn(getMockDatabaseData()).when(mockProductRepo).findAll();
         //Act
         testProductList = this.productService.displayAllProducts();
         //Assert
@@ -55,8 +73,10 @@ class ProductServiceTests {
     @Test
     public void Test_failToDisplayProductsDueToEmptyDatabase() {
         //Arrange
+        setMockDatabaseData(Collections.emptyList());
+        doReturn(getMockDatabaseData()).when(mockProductRepo).findAll();
         //Act
-        testProductList = mockProductService.displayAllProducts();
+        testProductList = productService.displayAllProducts();
         //Assert
         Assertions.assertFalse(testProductList.size() > 0);
     }
@@ -121,5 +141,13 @@ class ProductServiceTests {
 
     @Test
     public void Test_productRange() {
+    }
+
+    public List<Product> getMockDatabaseData() {
+        return mockDatabaseData;
+    }
+
+    public void setMockDatabaseData(List<Product> mockDatabaseData) {
+        this.mockDatabaseData = mockDatabaseData;
     }
 }
