fc273aaaa041bc5b80192025a0013ee4b911dd3f
Kollier Martin <kollierm@yahoo.com>
2021-12-01 17:00:44 -0500

    Optimized imports

--- user-service/src/main/java/com/revature/nova/utils/JWTUtil.java
+++ user-service/src/main/java/com/revature/nova/utils/JWTUtil.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
 import javax.annotation.PostConstruct;
 import java.security.Key;
 import java.util.Date;
