5e215ce4a535e164366117bdff842b6d437f6529
TCON163 <84452309+TCON163@users.noreply.github.com>
2021-11-30 18:31:55 -0600

    Forgot to add the imports
    Whoops

--- zuul-service/src/main/java/com/revature/nova/ZuulService.java
+++ zuul-service/src/main/java/com/revature/nova/ZuulService.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
