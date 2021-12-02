0e0aa71b6ca9ad711a5b2886373b066d42da0524
Kollier Martin <kollierm@yahoo.com>
2021-12-02 16:16:15 -0500

    Updates Added ID generation back to Logger Fixed ZUUL CORSFilter problem with PostMan pings, changed AuthenticationFilter to reflect the changes and check logically for header cases

--- user-service/src/main/java/com/revature/nova/clients/ProductClient.java
+++ user-service/src/main/java/com/revature/nova/clients/ProductClient.java
@@ -2,11 +2,13 @@ package com.revature.nova.clients;
 
 import com.revature.nova.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -18,29 +20,8 @@ import java.util.List;
  * @date 11/26/2021
  * @author Kollier Martin
  */
-@FeignClient(name = "productFeignClient", url = "${product-service.url}")
+@FeignClient(name = "productFeignClient", url = "http://localhost:8082/product-service/")
 public interface ProductClient {
-    private final WebClient client;
-
-    @Autowired
-    public ProductClient(){
-        client = WebClient.builder()
-                .baseUrl("http://localhost:8090")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-    }
-
-    @GetMapping("/title/")
-
-    public List<Product> getProduct(String productTitle){
-        return client.get()
-                .uri("/Nova/title/" + productTitle)
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .retrieve()
-                .bodyToFlux(Product.class)
-                .buffer()
-                .collectList()
-                .block()
-                .get(0);
-    }
+    @GetMapping("/Nova/title/{productTitle}")
+    List<Product> getProduct(@PathVariable String productTitle);
 }
