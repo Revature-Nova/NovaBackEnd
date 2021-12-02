dccd9bf95a51fe386d42adc7f433677a1bf7ed15
TCON163 <tylerconnercareersearch@gmail.com>
2021-12-02 16:07:39 -0600

    run cors filter

--- zuul-service/src/main/java/com/revature/nova/ZuulService.java
+++ zuul-service/src/main/java/com/revature/nova/ZuulService.java
@@ -16,24 +16,25 @@ public class ZuulService {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulService.class, args);
+		corsFilter();
 	}
 	
-//@Bean
-//public CorsFilter corsFilter() {
-//    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//    final CorsConfiguration config = new CorsConfiguration();
-//    config.setAllowCredentials(true);
-//    config.addAllowedOrigin("*");
-//    config.addAllowedHeader("*");
-//    config.addAllowedMethod("OPTIONS");
-//    config.addAllowedMethod("HEAD");
-//    config.addAllowedMethod("GET");
-//    config.addAllowedMethod("PUT");
-//    config.addAllowedMethod("POST");
-//    config.addAllowedMethod("DELETE");
-//    config.addAllowedMethod("PATCH");
-//    source.registerCorsConfiguration("/**", config);
-//    return new CorsFilter(source);
-//}
+@Bean
+public static CorsFilter corsFilter() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    final CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("OPTIONS");
+    config.addAllowedMethod("HEAD");
+    config.addAllowedMethod("GET");
+    config.addAllowedMethod("PUT");
+    config.addAllowedMethod("POST");
+    config.addAllowedMethod("DELETE");
+    config.addAllowedMethod("PATCH");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+}
 
 }
