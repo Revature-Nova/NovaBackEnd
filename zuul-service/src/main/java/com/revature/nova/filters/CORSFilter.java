c3c9782c929da5c49f703e838236b72fba293922
andrewdpetersen <90648378+andrewdpetersen@users.noreply.github.com>
2021-12-02 15:42:43 -0600

    Update CORSFilter.java

--- zuul-service/src/main/java/com/revature/nova/filters/CORSFilter.java
+++ zuul-service/src/main/java/com/revature/nova/filters/CORSFilter.java
@@ -25,7 +25,7 @@ public class CORSFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Origin", "http://*");
         resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
         resp.setHeader("Access-Control-Allow-Headers", "Response-Type, Content-Type, Authorization, authorization, Authentication");
         chain.doFilter(req, resp);
