6a8b4c9a9d4f885447a7b22b2daba12e100d0952
TCON163 <tylerconnercareersearch@gmail.com>
2021-12-02 16:21:13 -0600

    test

--- zuul-service/src/main/java/com/revature/nova/ZuulService.java
+++ zuul-service/src/main/java/com/revature/nova/ZuulService.java
@@ -24,7 +24,7 @@ public static CorsFilter corsFilter() {
     final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     final CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
-    config.addAllowedOrigin("*");
+    
     config.addAllowedHeader("*");
     config.addAllowedMethod("OPTIONS");
     config.addAllowedMethod("HEAD");
