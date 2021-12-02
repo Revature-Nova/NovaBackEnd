f6a9a9595b877db8c1e5fca855f55c51f41802f6
TCON163 <tylerconnercareersearch@gmail.com>
2021-12-02 16:02:48 -0600

    new test

--- zuul-service/src/main/java/com/revature/nova/filters/CORSFilter.java
+++ zuul-service/src/main/java/com/revature/nova/filters/CORSFilter.java
@@ -1,35 +1,35 @@
 
-//package com.revature.nova.filters;
-//
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.util.Collections;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.function.Function;
-//import java.util.stream.Collectors;
-//
-///**
-// * Global configuration for a CORSFilter
-// *
-// * @date 11/22/2021
-// * @author Kollier Martin
-// */
-//@Component
-//public class CORSFilter extends OncePerRequestFilter {
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws ServletException, IOException {
-//        resp.setHeader("Access-Control-Allow-Origin", "*");
-//        resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
-//        resp.setHeader("Access-Control-Allow-Headers", "Response-Type, Content-Type, Authorization, authorization, Authentication");
-//        chain.doFilter(req, resp);
-//    }
-//}
+package com.revature.nova.filters;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Global configuration for a CORSFilter
+ *
+ * @date 11/22/2021
+ * @author Kollier Martin
+ */
+@Component
+public class CORSFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+        resp.setHeader("Access-Control-Allow-Headers", "Response-Type, Content-Type, Authorization, authorization, Authentication");
+        chain.doFilter(req, resp);
+    }
+}
 
