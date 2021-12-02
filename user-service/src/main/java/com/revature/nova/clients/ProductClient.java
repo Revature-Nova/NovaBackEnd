502b5f13d4c32dede9e272faee471731a0118bf2
Kollier Martin <kollierm@yahoo.com>
2021-12-02 10:39:57 -0500

    Updates in Progress

--- user-service/src/main/java/com/revature/nova/clients/ProductClient.java
+++ user-service/src/main/java/com/revature/nova/clients/ProductClient.java
@@ -2,9 +2,12 @@ package com.revature.nova.clients;
 
 import com.revature.nova.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
@@ -15,8 +18,8 @@ import java.util.List;
  * @date 11/26/2021
  * @author Kollier Martin
  */
-@Service
-public class ProductClient {
+@FeignClient(name = "productFeignClient", url = "${product-service.url}")
+public interface ProductClient {
     private final WebClient client;
 
     @Autowired
@@ -26,6 +29,8 @@ public class ProductClient {
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
+
+    @GetMapping("/title/")
 
     public List<Product> getProduct(String productTitle){
         return client.get()
